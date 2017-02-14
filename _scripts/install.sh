e5343f986393397faa351fbcc5774220c74a1354
Mark Ide <git@cranstonide.com>
2017-02-13 23:27:22 -0500

    Initial build.

---
+++ _scripts/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip install awscli
+
+apt-get -qq update
+
+sudo apt-get install -y --no-install-recommends texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra texlive-latex-recommended pgf
