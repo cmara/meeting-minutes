fecfa591a295a7775ba999a9a285df2255729ce9
Mark Ide <git@cranstonide.com>
2017-02-13 23:54:04 -0500

    Move minutes into their own directory

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,19 +4,19 @@ mkdir -p _build/
 
 # 2016-01-21
 # Takes two passes to get page number references correct.
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-01-21-board-meeting.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-01-21-board-meeting.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-01-21-business-meeting.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-01-21-business-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-01-21-board-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-01-21-board-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-01-21-business-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-01-21-business-meeting.tex
 
 # 2016-02-18
 # Takes two passes to get page number references correct.
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-02-18-business-meeting.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-02-18-business-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-02-18-business-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-02-18-business-meeting.tex
 
 # 2016-03-17
 # Takes two passes to get page number references correct.
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-board-meeting.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-board-meeting.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-business-meeting.tex
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-business-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-03-17-board-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-03-17-board-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-03-17-business-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build minutes/2016-03-17-business-meeting.tex
