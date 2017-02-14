b9f6f7681fbacc078dacddc3e4525fa620ca9336
Mark Ide <git@cranstonide.com>
2017-02-13 23:50:34 -0500

    Add 2016-03

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -13,3 +13,10 @@ pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-0
 # Takes two passes to get page number references correct.
 pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-02-18-business-meeting.tex
 pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-02-18-business-meeting.tex
+
+# 2016-03-17
+# Takes two passes to get page number references correct.
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-board-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-board-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-business-meeting.tex
+pdflatex -interaction=nonstopmode -halt-on-error -output-directory _build 2016-03-17-business-meeting.tex
--- listing.md
+++ listing.md
@@ -1,5 +1,6 @@
 # CMARA Meeting Minutes Downloads
 _(Latest on top, earliest on bottom)_
 
+- `2016-03-17` [Business Meeting](https://share.cranstonide.com/w1ide/cmara/meeting-minutes/2016-03-17-business-meeting.pdf) and [Board Meeting](https://share.cranstonide.com/w1ide/cmara/meeting-minutes/2016-03-17-board-meeting.pdf)
 - `2016-02-18` [Business Meeting](https://share.cranstonide.com/w1ide/cmara/meeting-minutes/2016-02-18-business-meeting.pdf)
 - `2016-01-21` [Business Meeting](https://share.cranstonide.com/w1ide/cmara/meeting-minutes/2016-01-21-business-meeting.pdf) and [Board Meeting](https://share.cranstonide.com/w1ide/cmara/meeting-minutes/2016-01-21-board-meeting.pdf)
