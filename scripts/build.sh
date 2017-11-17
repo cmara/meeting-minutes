c12a1fe71ca904734607500de1e166c38345847d
Mark Ide <git@cranstonide.com>
2017-11-17 15:07:49 -0500

    Fix the local build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,18 +2,18 @@
 
 set -o errexit
 
-TEX_FILES=$(ls minutes/*.tex)
+TEX_FILES=$(ls minutes/)
 mkdir -p output/
 
 for TEX_FILE in $TEX_FILES
 do
   PDF_FILE=${TEX_FILE//.tex/.pdf}
 
-  if [ "$TEX_FILE" -nt "$PDF_FILE" ];
+  if [ "minutes/$TEX_FILE" -nt "output/$PDF_FILE" ];
   then
     echo "Building $TEX_FILE..."
-    pdflatex -interaction=nonstopmode -halt-on-error -output-directory output $TEX_FILE > /dev/null
-    pdflatex -interaction=nonstopmode -halt-on-error -output-directory output $TEX_FILE > /dev/null
+    pdflatex -interaction=nonstopmode -halt-on-error -output-directory output "minutes/$TEX_FILE" > /dev/null
+    pdflatex -interaction=nonstopmode -halt-on-error -output-directory output "minutes/$TEX_FILE" > /dev/null
   else
     echo "Skipping build of $TEX_FILE (nothing to do)"
   fi
